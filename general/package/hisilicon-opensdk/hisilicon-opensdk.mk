e6dda9cce19986dbd0e1203f6932ad0883b091f8
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2024-11-26 07:50:15 +0100

    added imx335_fpv sensor

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -33,6 +33,7 @@ endef
 define HISILICON_OPENSDK_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/sensors
 	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_SOC_FAMILY)/sony_imx335/libsns_imx335.so $(TARGET_DIR)/usr/lib/sensors
+	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_SOC_FAMILY)/sony_imx335_fpv/libsns_imx335_fpv.so $(TARGET_DIR)/usr/lib/sensors
 	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_SOC_FAMILY)/sony_imx307/libsns_imx307.so $(TARGET_DIR)/usr/lib/sensors
 endef
 
